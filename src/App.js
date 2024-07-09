4b490e955ac5a420bf6683feb8d6e7e96090006a
Prashant Kumar Jha <prashantkumarjha.cse2022@nsec.ac.in>
2024-07-09 13:37:16 +0530

    16d

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 
 import { useState } from 'react';
 import './App.css';
-import About from './components/About';
+// import About from './components/About';
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
 import Alert from './components/Alert';
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-} from "react-router-dom";
+// import {
+//   BrowserRouter as Router,
+//   Route,
+//   Routes,
+// } from "react-router-dom";
 function App() {
   const [mode, setMode]= useState('light');
   const [alert,setAlert]= useState(null);
